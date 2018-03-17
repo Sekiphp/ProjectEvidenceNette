aa4879fcf0d41df7db93ebed0e8f3b829aba3c12
Sekiphp <seki@lostwar.cz>
2018-03-17 20:37:25 +0100

    Novy projekt - prepare

--- app/presenters/ProjectPresenter.php
+++ app/presenters/ProjectPresenter.php
@@ -3,6 +3,7 @@
 namespace App\Presenters;
 
 use Nette;
+use Nette\Application\UI\Form;
 
 
 class ProjectPresenter extends Nette\Application\UI\Presenter
@@ -15,6 +16,30 @@ class ProjectPresenter extends Nette\Application\UI\Presenter
 
     public function renderEdit(int $projectId) {
         $this->template->project = $this->database->table('projects')->get($projectId);
+    }
+
+    public function renderNew() {
+        // empty
+    }
+
+    protected function createComponentNewProjectForm() {
+        // priprava dat do selectu - asi by se to spravne melo delat jinak?
+        $select = [];
+        $projectTypes = $this->database->table('types');
+        foreach ($projectTypes as $pt) {
+            $select[$pt['id']] = $pt['name'];
+        }
+
+        $form = new Form; // means Nette\Application\UI\Form
+
+        $form->addText('name', 'Název projektu')->setRequired();
+        $form->addText('deadline', 'Termín dokončení')->setRequired();
+        $form->addSelect('type_id', 'Typ projektu', $select);
+        $form->addCheckbox('is_web', 'Webový projekt');
+
+        $form->addSubmit('send', 'Vytvořit projekt');
+
+        return $form;
     }
 
 }
